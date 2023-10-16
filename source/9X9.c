1da6de219ff99ea970ea68ac46de424e093c7108
Filieka <144932126+Filieka@users.noreply.github.com>
2023-10-16 11:59:43 +0800

    Update

--- source/9X9.c
+++ source/9X9.c
@@ -4,10 +4,7 @@ int main(){
     int i,j;
     for(i=1;i<10;i++){
         for(j=1;j<10;j++){
-            printf("%d x %d = %d",j,i,i*j);
-            if(i*j<10){
-                printf(" ");
-            }
+            printf("%d x %d =\t%d",j,i,i*j);
             printf("    ");
         }
         printf("\n");
