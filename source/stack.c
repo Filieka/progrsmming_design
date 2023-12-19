482e4de465e0622230e749d5ebfdb547ed000510
Filieka <144932126+Filieka@users.noreply.github.com>
2023-12-20 00:49:32 +0800

    Update stack.c

--- source/stack.c
+++ source/stack.c
@@ -1,61 +1,100 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define road x>=0&&x<n*n&&map[x]==0
+#define road top[1]>=0&&top[1]<n*n&&map[top[1]]==0
+#define end *top>0&&&&(*top<n||*top>n*(n-1)||*top%n==0||*top%n==n-1)
 
-int stap(int n,int* map,int x,int* memory,int* count){
-    int i;
-    if(x==n*n-1){
-        return 1;
-    }
+int check(int* memory,int n,int x){
+    int i,count=0;
     for(i=0;i<n*n;i++){
-        if(memory[i]==x){
+        if(memory[i]==-1){
             return 0;
         }
-    }
-    if(road){
-        memory[*count]=x;
-        (*count)++;
-        if((x+1)%n!=0){
-            if(stap(n,map,x+1,memory,count)){
+        if(memory[i]==x){
+            count++;
+            if(count==2){
                 return 1;
             }
-        }       
-        if(stap(n,map,x+n,memory,count)){
-            return 1;
-        }
-        if((x-1)%n!=n-1){
-            if(stap(n,map,x-1,memory,count)){
-                return 1;
-            }
-        }
-        if(stap(n,map,x-n,memory,count)){
-            return 1;
         }
     }
     return 0;
-}
+}  
 
 int main(){
     unsigned int n;
-    int i,k,num=0;
-    int* count=&num;
+    int i,enter;
     scanf("%d",&n);
     int* map=(int*)malloc(n*n*sizeof(int));
     int* memory=(int*)malloc(n*n*sizeof(int));
+    int* top=memory;
     for(i=0;i<n*n;i++){
         memory[i]=-1;
     }
     for(i=0;i<n*n;i++){
         scanf("%d",&map[i]);
     }
-    if(stap(n,map,0,memory,count)){
-        printf("Yes\n");
+    scanf("%d",&enter);
+    enter*=n;
+    scanf("%d",&i);
+    enter+=i;
+    *top=enter;
+    while(1){
+        top[1]=*top+1;
+        if(check(memory,n,top[1])){
+            top[1]=-1;
+        }
+        else if(road){
+            top++;
+            if(end){
+                break;
+            }
+            continue;
+        }
+        top[1]=*top+n;
+        if(check(memory,n,top[1])){
+            top[1]=-1;
+        }
+        else if(road){
+            top++;
+            if(end){
+                break;
+            }
+            continue;
+        }
+        top[1]=*top-1;
+        if(check(memory,n,top[1])){
+            top[1]=-1;
+        }
+        else if(road){
+            top++;
+            if(end){
+                break;
+            }
+            continue;
+        }
+        top[1]=*top-n;
+        if(check(memory,n,top[1])){
+            top[1]=-1;
+        }
+        else if(road){
+            top++;
+            if(end){
+                break;
+            }
+            continue;
+        }
+        map[*top]=1;
+        top--;
     }
-    else{
-        printf("No\n");
+    i=0;
+    while(memory[i]!=-1){
+        printf("%d %d\n",memory[i]/n,memory[i]%n);
+        i++;
     }
-
+    if(memory[0]==-1){
+        printf("No way out\n");
+    }  
     free(map);
+    free(memory);
 
     return 0;
 }
